2f2627cacc9f5698ec7f2860fd4eb2727de75b4c
Yuancheng Zhang <old.far.hot@gmail.com>
2016-05-11 10:41:11 -0500

    finished #92 and updated hw3-checklist

--- En/Week03-HW/92-reverse-linked-list-ii.cpp
+++ En/Week03-HW/92-reverse-linked-list-ii.cpp
@@ -12,3 +12,41 @@ Note:
 Given m, n satisfy the following condition:
 1 ≤ m ≤ n ≤ length of list.the list, only nodes itself can be changed.
 */
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseBetween(ListNode* head, int m, int n) {
+        if (!head || !head->next || m == n) {
+            return head;
+        }
+        ListNode* dummy = new ListNode(0);
+        ListNode* prev = dummy;
+        dummy->next = head;
+        for (int i = 0; i < m - 1; i++) {
+            prev = prev->next;
+        }
+        // prev: m-1
+        ListNode* start = prev;         // m-1
+        ListNode* end = prev->next;     // m
+        ListNode* curr = prev->next;    // m
+        for (int i = 0; i <= n - m; i++) {
+            ListNode* next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+        }
+        // prev: n; curr: n+1
+        end->next = curr;
+        start->next = prev;
+        return dummy->next;
+    }
+};
+
