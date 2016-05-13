d9e954165241a54452c3aba8b33054a28c511457
Yuancheng Zhang <old.far.hot@gmail.com>
2016-05-13 16:07:44 -0500

    updated w6 #121

--- En/Week06-HW/121-best-time-to-buy-and-sell-stock.cpp
+++ En/Week06-HW/121-best-time-to-buy-and-sell-stock.cpp
@@ -14,10 +14,9 @@ public:
             return 0;
         } 
         int minPrice = prices[0];
-        int profit = prices[0];
         int maxProfit = 0;
         for (int i = 1; i < prices.size(); i++) {
-            profit = prices[i] - minPrice;
+            int profit = prices[i] - minPrice;
             maxProfit = maxProfit > profit ? maxProfit : profit;
             minPrice = minPrice < prices[i] ? minPrice : prices[i];
         }
