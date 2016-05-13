87c985b14f446a7b66c6b4245375be04042b9372
Yuancheng Zhang <old.far.hot@gmail.com>
2016-05-13 12:46:12 -0500

    finished w6 #121

---
+++ En/Week06-HW/121-best-time-to-buy-and-sell-stock.cpp
@@ -0,0 +1,25 @@
+/*
+https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+
+Say you have an array for which the ith element is the price of a given stock on day i.
+
+If you were only permitted to complete at most one transaction (ie, buy one and sell one share of the stock), design an algorithm to find the maximum profit.
+ */
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        if (prices.size() == 0 || prices.size() == 1) {
+            return 0;
+        } 
+        int minPrice = prices[0];
+        int profit = prices[0];
+        int maxProfit = 0;
+        for (int i = 1; i < prices.size(); i++) {
+            profit = prices[i] - minPrice;
+            maxProfit = maxProfit > profit ? maxProfit : profit;
+            minPrice = minPrice < prices[i] ? minPrice : prices[i];
+        }
+        return maxProfit;
+    }
+};
